53620c54d41e96b140f211d7c95c86a28f6a91d7
Jfolmer <113907783+Jfolmer@users.noreply.github.com>
2023-02-25 13:08:04 +0100

    Updating

--- Galaga/Program.cs
+++ Galaga/Program.cs
@@ -7,9 +7,9 @@ namespace Galaga
     {
         static void Main(string[] args)
         {
-            // var windowArgs = new WindowArgs() { Title = "Galaga v0.1" };
-            // var game = new Game(windowArgs);
-            // game.Run();
+            var windowArgs = new WindowArgs() { Title = "Galaga v0.1" };
+            var game = new Game(windowArgs);
+            game.Run();
             
         }
     }
