1a219bbc08e708343d8a50b717a6e98d22d5e84c
Jfolmer <113907783+Jfolmer@users.noreply.github.com>
2023-02-25 14:25:58 +0100

    changes

--- Galaga/Game.cs
+++ Galaga/Game.cs
@@ -4,19 +4,32 @@ using DIKUArcade.Graphics;
 using DIKUArcade.Math;
 using DIKUArcade;
 using DIKUArcade.GUI;
+
 namespace Galaga;
+
 public class Game : DIKUGame {
+
     private Player player;
+
     public Game(WindowArgs windowArgs) : base(windowArgs) {
+
         player = new Player(
+
         new DynamicShape(new Vec2F(0.45f, 0.1f), new Vec2F(0.1f, 0.1f)),
+
         new Image(Path.Combine("Assets", "Images", "Player.png")));
+
     }
     public override void Render() {
+
         //TODO: Render Game Entities
+
         throw new System.NotImplementedException("Galaga game has nothing to render yet.");
+
     }
     public override void Update() {
+
         throw new System.NotImplementedException("Galaga game has no entities to update yet.");
+        
     }
 }
